63a21122e3cf6f1afb743a2d8e8b8d1dc51545b1
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-10 17:08:47 +0200

    finito

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-function raccogliInput(clickEvent) {
-  console.log(clickEvent);
+function prendiDatiUtente() {
+  let nome = document.getElementById("nome").value;
+  let cognome = document.getElementById("cognome").value;
+  let data = document.getElementById("data").value;
+  let nuovoUtente = new Utente(nome, cognome, data);
+  nuovoUtente.createDiv();
 }
 
 class Utente {
-  constructor(nome, cognome, anno) {
+  constructor(nome, cognome, data) {
     this.nome = nome;
     this.cognome = cognome;
-    this.anno = anno;
+    this.anno = data;
   }
   createDiv = () => {
     let div = document.createElement("div");
     div.classList.add("utente");
-    div.innerHTML = this.nome + this.cognome + this.cognome;
-    return div;
+    div.innerHTML = this.nome + this.cognome + this.anno;
+    document.getElementById("container").appendChild(div);
   };
 }
